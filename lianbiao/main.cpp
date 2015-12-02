8698cd6c90a50194f0818940ff87faf0251a1736
20141105052 <xiaoods3on1@sina.com>
2015-12-02 20:57:41 +0800

    链表原始代码
    通过使用单向链表，定义数组，利用指针和循环实现输出四个学生数据，链表结构可以充分利用计算机内存空间，实现灵活的内存动态管理

--- lianbiao/main.cpp
+++ lianbiao/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+struct student
+{
+    char name[100];
+    //char sex;
+    int age;
+    student *next;
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //student A,B,C,D;
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s%d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    
+    //scanf("%s%d",B.name,&B.age);
+    //scanf("%s%d",C.name,&C.age);
+    //scanf("%s%d",D.name,&D.age);
+    //printf("&A=%x,&B=%x,&C=%x,&D=%x\n",&A,&B,&C,&D);
+    //A.next=&B;
+    //B.next=&C;
+    //C.next=&D;
+    //D.next=0;
+    /*printf("%s %d\n",A.name,A.age);
+    printf("%s %d\n",B.name,B.age);
+    printf("%s %d\n",C.name,C.age);
+    printf("%s %d\n",D.name,D.age);*/
+    //printf("%x   ,%x   ,%x,   %x",&A,&B,&C,&D);
+    //head=&A;
+    //p=head;
+    while(p!=0)
+    {
+        printf("%s %d  p=%x\n",p->name,p->age,p);
+        p=p->next;
+    }
     return 0;
 }
